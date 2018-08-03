f66be054f7394e52b5e41cfb614a8dd6fb3e8fcc
Jacob Matthews <jamatthews@users.noreply.github.com>
2018-08-03 23:41:20 +0200

    Automatic Priority [ch5784]

--- lib/sidekiq/priority/client.rb
+++ lib/sidekiq/priority/client.rb
@@ -5,14 +5,30 @@ module Sidekiq
       #inserted into Sidekiq's Client as middleware
       def call(worker_class, item, queue, redis_pool)
         if item['priority']
-          Sidekiq.redis do |conn|
-            queue = "priority-queue:#{queue}"
-            conn.zadd(queue, item['priority'], item.to_json)
-            return item
-          end
+          zadd(queue, item['priority'], item)
+        elsif item['prioritized_by']
+          prioritization_key = item['args'][item['prioritized_by']]
+          priority = fetch_and_add(queue, prioritization_key, item)
+          zadd(queue, priority, item)
         else
           # continue pushing the normal Sidekiq way
           yield
+        end
+      end
+
+      private
+
+      def zadd(queue, score, item)
+        Sidekiq.redis do |conn|
+          queue = "priority-queue:#{queue}"
+          conn.zadd(queue, score, item.to_json)
+          return item
+        end
+      end
+
+      def fetch_and_add(queue, prioritization_key, item)
+        Sidekiq.redis do |conn|
+          priority = conn.zincrby("priority-queue-counts:#{queue}", 1, prioritization_key)
         end
       end
     end
--- test/test_client.rb
+++ test/test_client.rb
@@ -8,18 +8,35 @@ class TestClient < Sidekiq::Test
       include Sidekiq::Worker
     end
 
-    it 'enqueues normally' do
+    it 'allows standard sidekiq functionality' do
       Sidekiq.redis {|c| c.flushdb }
       assert Worker.perform_async(1)
       assert_equal 1, Sidekiq::Queue.new().size
     end
 
-    it 'enqueues with a given priority' do
+    it 'pushes to a priority queue' do
       Sidekiq.redis {|c| c.flushdb }
       assert Worker.set(priority: 1).perform_async(1)
       assert_equal 1, Sidekiq::Priority::Queue.new().size
       job_count = Sidekiq.redis {|c| c.zcount('priority-queue:default', 1, 1) }
       assert_equal 1, job_count
+    end
+
+    class PrioritizedWorker
+      include Sidekiq::Worker
+
+      sidekiq_options prioritized_by: 0
+    end
+
+    it 'prioritises based on already enqueued jobs for the same key' do
+      Sidekiq.redis {|c| c.flushdb }
+      assert PrioritizedWorker.perform_async(1)
+      assert PrioritizedWorker.perform_async(1)
+      assert_equal 2, Sidekiq::Priority::Queue.new().size
+      job_count_p1 = Sidekiq.redis {|c| c.zcount('priority-queue:default', 1, 1) }
+      job_count_p2 = Sidekiq.redis {|c| c.zcount('priority-queue:default', 2, 2) }
+      assert_equal 1, job_count_p1
+      assert_equal 1, job_count_p2
     end
   end
 end
