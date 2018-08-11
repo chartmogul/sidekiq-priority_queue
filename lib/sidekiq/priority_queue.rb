645831019b4cd80c247eb9fd3c89f7177650519c
Jacob Matthews <jamatthews@users.noreply.github.com>
2018-08-11 16:35:05 +0200

    CombinedFetch [ch5925]

--- lib/sidekiq/priority_queue.rb
+++ lib/sidekiq/priority_queue.rb
@@ -3,6 +3,7 @@
 require 'sidekiq'
 require 'sidekiq/priority_queue/api'
 require 'sidekiq/priority_queue/client'
+require 'sidekiq/priority_queue/combined_fetch'
 require 'sidekiq/priority_queue/fetch'
 require 'sidekiq/priority_queue/reliable_fetch'
 require 'sidekiq/priority_queue/scripts'
