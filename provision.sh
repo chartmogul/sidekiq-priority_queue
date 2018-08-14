5ca5528d2a5e59422683cd05b8e4fc6e918f8b3e
Velina Rusjakova <velina.rusjakova@gmail.com>
2018-08-14 15:40:39 +0200

    Empty Rails app

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install redis-server ruby-dev build-essential zlib1g-dev
+sudo apt-get -y install redis-server ruby-dev build-essential zlib1g-dev libsqlite3-dev nodejs
 
 cd /vagrant
 sudo gem install bundler
