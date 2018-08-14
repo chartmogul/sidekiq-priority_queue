6f6e55ba20bd60c92f38ae485587f4da37173627
Velina Rusjakova <velina.rusjakova@gmail.com>
2018-08-14 15:04:26 +0200

    Add zlib lib

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install redis-server ruby-dev build-essential
+sudo apt-get -y install redis-server ruby-dev build-essential zlib1g-dev
 
 cd /vagrant
 sudo gem install bundler
