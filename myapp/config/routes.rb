d7346e10a6e89f17edb3cb0c8890e8fc3f36c033
Velina <velina.rusjakova@gmail.com>
2018-08-14 15:47:04 +0200

    Empty rails app (#6)

---
+++ myapp/config/routes.rb
@@ -0,0 +1,6 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
