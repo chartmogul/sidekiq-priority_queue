1613852173a6cf38f6c46da6a4dff19d3320bee8
Velina <velina.rusjakova@gmail.com>
2018-08-17 10:06:20 +0200

    Web UI [ch5783] (#7)

--- lib/sidekiq/priority_queue/api.rb
+++ lib/sidekiq/priority_queue/api.rb
@@ -1,15 +1,64 @@
 # frozen_string_literal: true
+require 'sidekiq/api'
+
 
 module Sidekiq
   module PriorityQueue
+
+    module RedisScanner
+      def redis_scan(conn, pattern)
+        cursor = '0'
+        result = []
+        loop do
+          cursor, values = conn.scan(cursor, match: pattern)
+          result.push(*values)
+          break if cursor == '0'
+        end
+        result
+      end
+    end
+
     class Queue
-      def initialize(name="default")
+      extend PriorityQueue::RedisScanner
+
+      attr_reader :name
+
+      def initialize(name='default')
         @name = name
         @rname = "priority-queue:#{name}"
       end
 
       def size
         Sidekiq.redis { |con| con.zcard(@rname) }
+      end
+
+      def self.all
+         Sidekiq.redis { |con| redis_scan(con, 'priority-queue:*') }
+          .map{ |key| key.gsub('priority-queue:', '') }
+          .sort
+          .map { |q| Queue.new(q) }
+      end
+    end
+
+    class Job < Sidekiq::Job
+
+      attr_reader :priority
+      attr_reader :subqueue
+      
+      def initialize(item, queue_name = nil, priority = nil)
+        @args = nil
+        @value = item
+        @item = item.is_a?(Hash) ? item : parse(item)
+        @queue = queue_name || @item['queue']
+        @subqueue = @item['subqueue']
+        @priority = priority
+      end
+
+      def delete
+        count = Sidekiq.redis do |conn|
+          conn.zrem("priority-queue:#{@queue}", @value)
+        end
+        count != 0
       end
     end
   end
