eba7af37d223a0c6422ffa125c22c1c823898829
Jacob Matthews <jamatthews@users.noreply.github.com>
2018-09-03 15:29:01 +0200

    add LICENCE and attributions

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,9 @@
+# Copyright (c) Contributed Systems LLC, , ChartMogul Ltd
+
+# Sidekiq-priority_queue is an Open Source project licensed under the terms of
+# the LGPLv3 license.  Please see <http://www.gnu.org/licenses/lgpl-3.0.html>
+# for license text.
+
 # frozen_string_literal: true
 $TESTING = true
 # disable minitest/parallel threads
