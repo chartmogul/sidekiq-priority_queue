e1a7d8e713d27c7f05d569cd2b726ef3bf2f5c93
Petr Kopac <pkopac@users.noreply.github.com>
2020-05-27 09:48:47 +0200

    Avoid useless security warnings [ch22233] (#34)
    * Remove sample application
    
    * build update; deps updates
    
    * version 0.1.0, email update

--- myapp/config/routes.rb
+++
@@ -1,6 +0,0 @@
-require 'sidekiq/web'
-
-Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
