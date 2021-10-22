172d2eddf1d5cff22e7a4b58a4f99e1c50660343
azranel <bart@chartmogul.com>
2021-10-22 18:16:39 +0200

    Update ReliableFetch to handle dead jobs

--- lib/sidekiq/priority_queue/reliable_fetch.rb
+++ lib/sidekiq/priority_queue/reliable_fetch.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 require 'sidekiq'
+require 'sidekiq/util'
 
 module Sidekiq
   module PriorityQueue
     class ReliableFetch
+      include Sidekiq::Util
 
       UnitOfWork = Struct.new(:queue, :job, :wip_queue) do
         def acknowledge
@@ -38,10 +40,26 @@ module Sidekiq
         @strictly_ordered_queues = !!options[:strict]
         @queues = options[:queues].map { |q| "priority-queue:#{q}" }
         @queues = @queues.uniq if @strictly_ordered_queues
+        @done = false
         @process_index = options[:index] || ENV['PROCESS_INDEX']
       end
 
+      def setup
+        Sidekiq.on(:startup) do
+          cleanup_the_dead
+          register_myself
+        end
+        Sidekiq.on(:shutdown) do
+          @done = true
+        end
+        Sidekiq.on(:heartbeat) do
+          register_myself
+        end
+      end
+
       def retrieve_work
+        return nil if @done
+
         work = @queues.detect do |q|
           job = zpopmin_sadd(q, wip_queue(q));
           break [q,job] if job
@@ -50,7 +68,7 @@ module Sidekiq
       end
 
       def wip_queue(q)
-        "#{q}_wip_#{@process_index}"
+        "queue:spriorityq|#{identity}|#{q}"
       end
 
       def zpopmin_sadd(queue, wip_queue)
@@ -72,55 +90,102 @@ module Sidekiq
         end
       end
 
-      def bulk_requeue(_inprogress, options)
-        Sidekiq.logger.debug { "Re-queueing terminated jobs" }
-        process_index = options[:index] || ENV['PROCESS_INDEX']
-        self.class.requeue_wip_jobs(options[:queues], process_index)
-      end
-
-      def self.resume_wip_jobs(queues, process_index)
-        Sidekiq.logger.debug { "Re-queueing WIP jobs" }
-        process_index ||= ENV['PROCESS_INDEX']
-        requeue_wip_jobs(queues, process_index)
-      end
-
-      Sidekiq.configure_server do |config|
-        config.on(:startup) do
-          if reliable_fetch_active?(config)
-            Sidekiq::PriorityQueue::ReliableFetch.resume_wip_jobs(config.options[:queues], config.options[:index])
-          end
-        end
+      def bulk_requeue(_inprogress, _options)
+        Sidekiq.logger.debug { "Priority ReliableFetch: Re-queueing terminated jobs" }
+        requeue_wip_jobs
+        unregister_super_process
       end
 
       private
 
-      def self.reliable_fetch_active?(config)
-        return true if config.options[:fetch].is_a?(Sidekiq::PriorityQueue::ReliableFetch)
-        return config.options[:fetch].is_a?(Sidekiq::PriorityQueue::CombinedFetch) &&
-          config.options[:fetch].fetches.any? { |f| f.is_a?(Sidekiq::PriorityQueue::ReliableFetch) }
+      def cleanup_the_dead
+        overall_moved_count = 0
+        Sidekiq.redis do |conn|
+          conn.sscan_each("super_processes_priority") do |x|
+            next if conn.exists?(x) # Don't cleanup currently running processes
+
+            Sidekiq.logger.debug { "Priority ReliableFetch: Moving job from #{x} back to original queues" }
+
+            # We need to pushback any leftover jobs still in WIP
+            previously_handled_queues = conn.smembers("#{x}:super_priority_queues")
+
+            # Below previously_handled_queues are simply WIP queues of previous, dead processes
+            previously_handled_queues.each do |previously_handled_queue|
+              queue_moved_size = 0
+              original_priority_queue_name = previously_handled_queue.split('|').last
+
+              Sidekiq.logger.debug { "Priority ReliableFetch: Moving job from #{previously_handled_queue} back to original queue: #{original_priority_queue_name}" }
+              loop do
+                break if conn.scard(previously_handled_queue) == 0
+
+                item = conn.spop(previously_handled_queue)
+                conn.zadd(original_priority_queue_name, 0, item)
+                queue_moved_size += 1
+                overall_moved_count += 1
+              end
+              Sidekiq.logger.debug { "Priority ReliableFetch: Moved #{queue_moved_size} jobs from ##{previously_handled_queue} back to original_queue: #{original_priority_queue_name} "}
+            end
+
+            Sidekiq.logger.debug { "Priority ReliableFetch: Unregistering super process #{x}" }
+            conn.del("#{x}:super_priority_queues")
+            conn.srem("super_processes_priority", x)
+          end
+        end
+        Sidekiq.logger.debug { "Priority ReliableFetch: Moved overall #{overall_moved_count} jobs from WIP queues" }
+      rescue => ex
+        # best effort, ignore Redis network errors
+        Sidekiq.logger.warn { "Priority ReliableFetch: Failed to requeue: #{ex.message}" }
       end
 
-      def self.requeue_wip_jobs(queues, index)
+      def requeue_wip_jobs
         jobs_to_requeue = {}
         Sidekiq.redis do |conn|
-          queues.map { |q| "priority-queue:#{q}" }.each do |q|
-            wip_queue = "#{q}_wip_#{index}"
+          @queues.each do |q|
+            wip_queue_name = wip_queue(q)
             jobs_to_requeue[q] = []
-            while job = conn.spop(wip_queue) do
+
+            while job = conn.spop(wip_queue_name) do
               jobs_to_requeue[q] << job
             end
           end
 
           conn.pipelined do
             jobs_to_requeue.each do |queue, jobs|
-              return unless jobs.size > 0
-              conn.zadd(queue, jobs.map{|j| [0,j] })
+              next if jobs.size == 0 # ZADD doesn't work with empty arrays
+
+              conn.zadd(queue, jobs.map {|j| [0, j] })
             end
           end
         end
-        Sidekiq.logger.info("Pushed #{ jobs_to_requeue.map{|q| q.size }.reduce(:+) } jobs back to Redis")
+        Sidekiq.logger.info("Priority ReliableFetch: Pushed #{ jobs_to_requeue.values.flatten.size } jobs back to Redis")
       rescue => ex
-        Sidekiq.logger.warn("Failed to requeue #{ jobs_to_requeue.map{|q| q.size }.reduce(:+) } jobs: #{ex.message}")
+        Sidekiq.logger.warn("Priority ReliableFetch: Failed to requeue #{ jobs_to_requeue.values.flatten.size } jobs: #{ex.message}")
+      end
+
+      def register_myself
+        qs = @queues.map { |q| wip_queue(q) }
+        id = identity # This is from standard sidekiq, updated with every heartbeat
+
+        # This method will run multiple times so seeing this message twice is no problem.
+        Sidekiq.logger.debug { "Priority ReliableFetch: Registering super process #{id} with #{qs}" }
+
+        Sidekiq.redis do |conn|
+          conn.multi do
+            conn.sadd("super_processes_priority", id)
+            conn.sadd("#{id}:super_priority_queues", qs)
+          end
+        end
+      end
+
+      def unregister_super_process
+        id = identity
+        Sidekiq.logger.debug { "Priority ReliableFetch: Unregistering super process #{id}" }
+        Sidekiq.redis do |conn|
+          conn.multi do
+            conn.srem("super_processes_priority", id)
+            conn.del("#{id}:super_priority_queues")
+          end
+        end
       end
     end
   end
--- test/test_reliable_fetch.rb
+++ test/test_reliable_fetch.rb
@@ -2,7 +2,8 @@
 require_relative 'helper'
 
 class TestFetcher < Sidekiq::Test
-  describe 'reiable fetcher' do
+
+  describe 'reliable fetcher' do
     job = {'jid' => 'blah', 'args' => [1,2,3], 'subqueue' => 1 }
 
     before do
@@ -12,38 +13,114 @@ class TestFetcher < Sidekiq::Test
         conn.zadd('priority-queue:foo', 0, job.to_json)
         conn.zadd("priority-queue-counts:foo", 1, job['subqueue'])
       end
+      reset_sidekiq_lifecycle_events
     end
 
     after do
       Sidekiq.redis = REDIS
     end
 
-    it 'retrieves and puts into private set' do
+    it 'adds three new event triggers when #setup is called' do
+      assert_equal 0, Sidekiq.options[:lifecycle_events].values.flatten.size
       fetch = Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['foo'], index: 0)
+      fetch.setup
+      assert_equal 3, Sidekiq.options[:lifecycle_events].values.flatten.size
+    end
+
+    it 'stops picking up work once shutdown event is triggered' do
+      fetch = Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['foo'], index: 0)
+      fetch.setup
+
+      SidekiqUtilInstance.new.fire_event(:shutdown)
+
+      assert_equal 1, Sidekiq::PriorityQueue::Queue.new('foo').size
+      assert_nil fetch.retrieve_work
+    end
+
+    it 'cleanups dead jobs on startup' do
+      # First we have to mimic old jobs laying around
+      previous_process_identity = "sidekiq-pipeline-32152-xfwvw:42251:#{SecureRandom.hex(6)}"
+      priority_queue = "priority-queue:bar"
+
+      Sidekiq.redis do |conn|
+        conn.sadd("super_processes_priority", previous_process_identity)
+        previous_wip_queue = "queue:spriorityq|#{previous_process_identity}|#{priority_queue}"
+        conn.sadd("#{previous_process_identity}:super_priority_queues", previous_wip_queue)
+        conn.sadd(previous_wip_queue, job.to_json)
+      end
+
+      # Then we setup the fetcher
+      fetch = Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['bar'], index: 0)
+      fetch.setup
+
+      # And here we test is really on startup it brings back old jobs
+      assert_equal 0, Sidekiq::PriorityQueue::Queue.new('bar').size
+      SidekiqUtilInstance.new.fire_event(:startup)
+      assert_equal 1, Sidekiq::PriorityQueue::Queue.new('bar').size
+    end
+
+    it 'registers process and private queues on startup' do
+      fetch = Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['foo'], index: 0)
+      fetch.setup
+
+      SidekiqUtilInstance.new.fire_event(:startup)
+
+      Sidekiq.redis do |conn|
+        registered_processes = conn.smembers("super_processes_priority")
+        assert_equal 1, registered_processes.size
+        assert_equal fetch.identity, registered_processes.first
+        private_queues = conn.smembers("#{registered_processes.first}:super_priority_queues")
+        assert_equal 1, private_queues.size
+        identity = registered_processes.first
+        assert_equal "queue:spriorityq|#{identity}|priority-queue:foo", private_queues.first
+      end
+    end
+
+    it 'registers process and private queues on heartbeat' do
+      fetch = Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['foo'], index: 0)
+      fetch.setup
+
+      SidekiqUtilInstance.new.fire_event(:heartbeat)
+
+      Sidekiq.redis do |conn|
+        registered_processes = conn.smembers("super_processes_priority")
+        assert_equal 1, registered_processes.size
+        assert_equal fetch.identity, registered_processes.first
+        private_queues = conn.smembers("#{registered_processes.first}:super_priority_queues")
+        assert_equal 1, private_queues.size
+        identity = registered_processes.first
+        assert_equal "queue:spriorityq|#{identity}|priority-queue:foo", private_queues.first
+      end
+    end
+
+    it 'retrieves and puts into private set' do
+      fetch = setup_reliable_fetcher
       uow = fetch.retrieve_work
       refute_nil uow
       assert_equal 'foo', uow.queue_name
       assert_equal job.to_json, uow.job
-      Sidekiq.redis do |conn|
-        assert conn.sismember("priority-queue:foo_wip_0", job.to_json)
-      end
+      identity = fetch.identity
+      wip_queue = "queue:spriorityq|#{identity}|priority-queue:foo"
+      Sidekiq.redis { |conn| assert conn.sismember(wip_queue, job.to_json) }
       q = Sidekiq::PriorityQueue::Queue.new('foo')
       assert_equal 0, q.size
       assert uow.acknowledge
       Sidekiq.redis do |conn|
         assert_nil conn.zscore("priority-queue-counts:foo", job['subqueue'])
-        assert !conn.sismember("priority-queue:foo_wip_0", job.to_json)
+        assert !conn.sismember(wip_queue, job.to_json)
       end
     end
 
-    it 'pushes WIP jobs back to the head of the queue' do
-      killed_job = {'jid' => 'blah_blah', 'args' => [1,2,3], 'subqueue' => 1 }
-      Sidekiq.redis do |conn|
-        conn.sadd("priority-queue:foo_wip_0", killed_job.to_json)
-      end
+    it 'pushes WIP jobs back to the head of the sorted set' do
+      assert_equal 1, Sidekiq::PriorityQueue::Queue.new('foo').size
+      fetch = setup_reliable_fetcher
+      identity = fetch.identity
+      wip_queue = "queue:spriorityq|#{identity}|priority-queue:foo"
 
-      fetch = Sidekiq::PriorityQueue::ReliableFetch
-      fetch.resume_wip_jobs(['foo'], 0)
+      killed_job = {'jid' => 'blah_blah', 'args' => [1,2,3], 'subqueue' => 1 }
+      Sidekiq.redis { |conn| conn.sadd(wip_queue, killed_job.to_json) }
+
+      fetch.bulk_requeue(nil, nil)
       assert_equal 2, Sidekiq::PriorityQueue::Queue.new('foo').size
     end
 
@@ -51,26 +128,6 @@ class TestFetcher < Sidekiq::Test
       fetch = Sidekiq::PriorityQueue::ReliableFetch.new(:queues => ['basic', 'bar', 'bar'], :strict => true)
       cmd = fetch.queues_cmd
       assert_equal cmd, ['priority-queue:basic', 'priority-queue:bar']
-    end
-
-    it 'bulk requeues only priority-queue jobs' do
-      q1 = Sidekiq::PriorityQueue::Queue.new('foo')
-      q2 = Sidekiq::PriorityQueue::Queue.new('bar')
-      assert_equal 1, q1.size
-      assert_equal 0, q2.size
-      uow = Sidekiq::PriorityQueue::Fetch::UnitOfWork
-
-      Sidekiq.redis do |conn|
-        conn.sadd("priority-queue:foo_wip_0", 'bob')
-      end
-
-      Sidekiq::PriorityQueue::ReliableFetch.new(queues: ['foo'], index: 0).bulk_requeue(
-        [ uow.new('priority-queue:foo', 'bob'), uow.new('fuzzy:queue:foo', 'bar') ],
-        { queues: ['foo'], index: 0 }
-      )
-
-      assert_equal 2, q1.size
-      assert_equal 0, q2.size
     end
   end
 end
